74107f437d4559b1ad6801134525323aff4189ee
Manuel Torrez <mastervado98@gmail.com>
2018-02-13 15:06:48 -0600

    Implement edit and delete

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Manuel", email: "manuel@example.com")
+    @recipe = Recipe.create(name: "boiled water", description: "great boiled water", chef: @chef)
+  end
+
+  test "successfully delete a recipe" do 
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select "a[href=?]", recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path(@recipes)
+    assert_not flash.empty?
+  end
+end
---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @chef = Chef.create!(chefname: "Manuel", email: "manuel@example.com")
+    @recipe = Recipe.create(name: "boiled water", description: "great boiled water", chef: @chef)
+  end
+
+  test "reject invalid recipe update" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: { recipe: { name: " ", description: "some description" } }
+    assert_template 'recipes/edit'
+    assert_select 'h2.card-title'
+    assert_select 'div.card-block'
+  end
+
+  test "Successfully edit a recipe" do 
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_recipe_description = "updated recipe description"
+    patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_recipe_description } }
+    assert_redirected_to @recipe
+    #follow_redirect!    -it's the same as assert_redirect_to
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_recipe_description, @recipe.description
+  end
+
+end
