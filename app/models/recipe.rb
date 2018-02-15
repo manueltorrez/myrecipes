4f0698b5d1854d9928f147eaba4626f6d59974ba
Manuel Torrez <mastervado98@gmail.com>
2018-02-15 16:09:32 -0600

    Max length fixed on recipe name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, length: { maximum: 60 }
     validates :description, presence: true, length: { minimum: 5, maximum: 500 }
     validates :chef_id, presence: true
     belongs_to :chef
