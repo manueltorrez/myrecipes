a11337160c97c5cf4a2925f732af8e13dec19859
Manuel Torrez <mastervado98@gmail.com>
2018-02-26 15:09:52 -0600

    Add chefs destroy and tests

--- test/integration/chefs_index_test_test.rb
+++ test/integration/chefs_index_test_test.rb
@@ -19,4 +19,14 @@ class ChefsIndexTestTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", chef_path(@chef), text: @chef.chefname.capitalize
   end
 
+  test 'should delete chef' do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end
+
 end
